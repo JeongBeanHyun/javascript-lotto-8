c58304d65150d3530fb05e4b45e35b8f7c517969
JeongBeanHyun <kelly92108@gmail.com>
2025-11-02 04:37:30 +0900

    test(__test__) : 로또 생성 테스트 작성

---
+++ __tests__/LottoGeneratorTest.js
@@ -0,0 +1,35 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import LottoGenerator from "../src/LottoGenerator.js";
+import Lotto from "../src/Lotto.js";
+
+jest.mock("@woowacourse/mission-utils");
+
+describe("LottoGenerator 테스트", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("generateOne()은 Lotto 객체를 반환한다", () => {
+    MissionUtils.Random.pickUniqueNumbersInRange.mockReturnValue([
+      1, 2, 3, 4, 5, 6,
+    ]);
+
+    const lotto = LottoGenerator.generateOne();
+
+    expect(lotto).toBeInstanceOf(Lotto);
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test("generateMany()는 원하는 개수만큼 Lotto 객체를 생성한다", () => {
+    MissionUtils.Random.pickUniqueNumbersInRange.mockReturnValue([
+      1, 2, 3, 4, 5, 6,
+    ]);
+
+    const tickets = LottoGenerator.generateMany(3);
+
+    expect(tickets).toHaveLength(3);
+    tickets.forEach((ticket) => {
+      expect(ticket).toBeInstanceOf(Lotto);
+    });
+  });
+});
