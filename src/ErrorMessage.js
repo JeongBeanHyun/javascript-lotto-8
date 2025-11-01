8bb622ab44a7afd6da23ee1141b3a9bb2ca74065
JeongBeanHyun <kelly92108@gmail.com>
2025-11-02 03:27:46 +0900

    feat : 에러메시지 상수 객체 추가

---
+++ src/ErrorMessage.js
@@ -0,0 +1,8 @@
+export const ERROR_MESSAGE = {
+  INVALID_LOTTO_LENGTH: "[ERROR] 로또 번호는 6개입니다.",
+  INVALID_RANGE: "[ERROR] 로또 번호 범위는 1~45 입니다.",
+  DUPLICATE_LOTTO_NUMBER: "[ERROR] 로또 번호는 중복될 수 없습니다.",
+  INVALID_SEPARATOR: "[ERROR] 구분자는 쉼표(,)만 가능합니다.",
+  INVALID_AMOUNT_UNIT: "[ERROR] 구입 금액의 단위는 1,000원입니다.",
+  INVALID_AMOUNT_POSITIVE: "[ERROR] 구입 금액은 양의 정수입니다.",
+};
