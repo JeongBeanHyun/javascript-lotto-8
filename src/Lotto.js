5032022ef2c725818589217d3b1a415e28214a2a
JeongBeanHyun <kelly92108@gmail.com>
2025-11-02 04:16:31 +0900

    feat : 로또 검증 예외처리 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,32 @@
+import { ERROR_MESSAGE } from "./errorMessage";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = [...numbers].sort((a, b) => a - b);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR_MESSAGE.INVALID_LOTTO_LENGTH);
+    }
+    if (numbers.some((n) => n < 1 || n > 45)) {
+      throw new Error(ERROR_MESSAGE.INVALID_RANGE);
+    }
+
+    const duplicates = numbers.filter(
+      (num, index) => numbers.indexOf(num) != index
+    );
+    if (duplicates.length > 0) {
+      throw new Error(ERROR_MESSAGE.DUPLICATE_LOTTO_NUMBER);
     }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
