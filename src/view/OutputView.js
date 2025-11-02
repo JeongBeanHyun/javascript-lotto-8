d4bfcf09f2fc275f55a6047fd64df82ead643209
JeongBeanHyun <kelly92108@gmail.com>
2025-11-02 20:17:34 +0900

    feat(outputview) : 자동 발매된 로또,  로또 발매 횟수 출력 기능 추가 및 오타 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -2,23 +2,40 @@ import { Console } from "@woowacourse/mission-utils";
 
 class OutputView {
   static printResult(summary) {
-    Console.print("당첨 통계\n");
-    Console.print("---\n");
+    Console.print("\n당첨 통계");
+    Console.print("---");
 
     const result = [
       { label: "3개 일치", prize: "5,000원", rank: 5 },
       { label: "4개 일치", prize: "50,000원", rank: 4 },
       { label: "5개 일치", prize: "1,500,000원", rank: 3 },
-      { label: "5개 일치, 보너스 볼 일치", prize: "30,000,000", rank: 2 },
-      { label: "6개 일치", prize: "2,000,000,000", rank: 1 },
+      {
+        label: "5개 일치, 보너스 볼 일치",
+        prize: "30,000,000원",
+        rank: 2,
+      },
+      { label: "6개 일치", prize: "2,000,000,000원", rank: 1 },
     ];
 
     result.forEach(({ label, prize, rank }) => {
-      Console.print(`${label} (${prize}) - ${summary[rank]}개\n`);
+      Console.print(`${label} (${prize}) - ${summary[rank]}개`);
     });
   }
 
-  static printEaringRate(rate) {
+  static printPurchase(count) {
+    Console.print("\n" + count + "개를 구매했습니다.");
+  }
+
+  static printTickets(tickets) {
+    for (const t of tickets) {
+      const numbers = t.getNumbers();
+      Console.print("[" + numbers.join(", ") + "]");
+    }
+  }
+
+  static printEarningRate(rate) {
     Console.print(`총 수익률은 ${rate}%입니다.`);
   }
 }
+
+export default OutputView;
