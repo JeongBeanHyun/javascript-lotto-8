4637ae81b1cdc154d3eb8d46cd8f19724f1b969f
JeongBeanHyun <kelly92108@gmail.com>
2025-11-02 20:18:40 +0900

    feat(inputview) : 입력 메시지 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,17 +2,19 @@ import { Console } from "@woowacourse/mission-utils";
 
 class InputView {
   static async inputPurchaseAmount() {
-    const input = await Console.readLineAsync("구입 금액을 입력해 주세요.\n");
+    const input = await Console.readLineAsync("\n구입금액을 입력해 주세요.\n");
     return input.trim();
   }
 
   static async inputWinningNumbers() {
-    const input = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+    const input = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
     return input.trim();
   }
 
   static async inputBonusNumber() {
-    const input = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+    const input = await Console.readLineAsync(
+      "\n보너스 번호를 입력해 주세요.\n"
+    );
     return input.trim();
   }
 }
