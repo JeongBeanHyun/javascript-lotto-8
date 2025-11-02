5e74a426d1b123f2f411dd508e025a2f30ac069a
JeongBeanHyun <kelly92108@gmail.com>
2025-11-02 20:27:46 +0900

    feat(lottocontroller) : 로또 컨트롤러 추가 및 app에 컨트롤러 생성

---
+++ src/LottoController.js
@@ -0,0 +1,65 @@
+import { Console } from "@woowacourse/mission-utils";
+import LottoGenerator from "./LottoGenerator.js";
+import PurchaseService from "./PurchaseService.js";
+import ResultService from "./ResultService.js";
+import InputView from "./view/InputView.js";
+import OutputView from "./view/outputView.js";
+import WinningNumbersService from "./WinningNumbersService.js";
+
+class LottoController {
+  async startLotto() {
+    const amount = await this.#askAmount();
+    const count = PurchaseService.calculateLottoCount(amount);
+    const tickets = LottoGenerator.generateMany(count);
+
+    OutputView.printPurchase(count);
+    OutputView.printTickets(tickets);
+
+    const winningNumbers = await this.#askWinningNumbers();
+    const bonusNumber = await this.#askBonusNumber(winningNumbers);
+
+    const result = tickets.map((t) => t.getNumbers());
+    const summary = ResultService.summarizeRanks(
+      result,
+      winningNumbers,
+      bonusNumber
+    );
+    const totalPrize = ResultService.calculateTotalPrize(summary);
+    const rate = ResultService.calculateEarningRate(totalPrize, amount);
+
+    OutputView.printResult(summary);
+    OutputView.printEarningRate(rate);
+  }
+
+  async #askAmount() {
+    try {
+      const amount = await InputView.inputPurchaseAmount();
+      return PurchaseService.validateAmount(amount);
+    } catch (error) {
+      Console.print(error.message);
+      return this.#askAmount();
+    }
+  }
+
+  async #askWinningNumbers() {
+    try {
+      const winningNumbers = await InputView.inputWinningNumbers();
+      return WinningNumbersService.parseWinningNumbers(winningNumbers);
+    } catch (error) {
+      Console.print(error.message);
+      return this.#askWinningNumbers();
+    }
+  }
+
+  async #askBonusNumber(winningNumbers) {
+    try {
+      const bonus = await InputView.inputBonusNumber();
+      return WinningNumbersService.parseBonusNumber(bonus, winningNumbers);
+    } catch (error) {
+      Console.print(error.message);
+      return this.#askBonusNumber(winningNumbers);
+    }
+  }
+}
+
+export default LottoController;
